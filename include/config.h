2f26e28daefee3e97662dda5886a7116caf20021
Pot4too <alexbaris167@gmail.com>
2025-07-02 11:39:29 +0200

    Updated config.h and added comments

--- include/config.h
+++ include/config.h
@@ -1,55 +1,13 @@
 #pragma once
-//! Debuging
+
+// comment out '#define DEBUG at the top of the config.h file' to disable debug mode
 #define DEBUG
-
-#ifdef DEBUG
-#define DEBUG_PRINT(x) Serial.print(x)
-#define DEBUG_PRINTLN(x) Serial.println(x)
-#else
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINTLN(x)
-#endif
-
-//! Pin configuration
-//* I2C Pins
-#define I2C_SCL_PIN 22
-#define I2C_SDA_PIN 21
-
-//* SPI Pins
-#define SPI_MOSI_PIN 23
-#define SPI_MISO_PIN 19
-#define SPI_SCK_PIN 18
-#define SD_CS_PIN 5
-
-//* Analog Sensor Pins
-//? Pin definitions
-#define A0_PIN 36
-#define A1_PIN 39
-
-//? Pin Sensor Definitions
-#define A0_Sensor CapacitiveMoisture
-#define A1_Sensor MAX4466
-
-//* Digital Pins
-//? Pin definitions
-#define D0_PIN -1
-#define D1_PIN -1
-#define D2_PIN -1
-
-//? Pin Sensor Definitions
-#define D0_Sensor AHT20
-
-//* BMP280 sensor
-#define seaLevelPressure 1013.25 // Default sea level pressure in hPa
-
-#pragma once
-#define DEBUG // ✅ For preprocessor conditionals (e.g., #ifdef DEBUG)
 
 namespace Config
 {
-
+//! Debug configuration
 #ifdef DEBUG
-    constexpr bool DEBUG_MODE = true; // ✅ Optional: for C++ logic
+    constexpr bool DEBUG_MODE = true;
 #define DEBUG_PRINT(x) Serial.print(x)
 #define DEBUG_PRINTLN(x) Serial.println(x)
 #else
@@ -58,11 +16,8 @@ namespace Config
 #define DEBUG_PRINTLN(x)
 #endif
 
+    //! Global definitions
     constexpr int SERIAL_BAUD_RATE = 115200;
-
-    //* I2C
-    constexpr int I2C_SDA_PIN = 21;
-    constexpr int I2C_SCL_PIN = 22;
 
     //* SPI
     constexpr int SPI_MOSI_PIN = 23;
@@ -70,10 +25,11 @@ namespace Config
     constexpr int SPI_SCK_PIN = 18;
     constexpr int SD_CS_PIN = 5; // SD ChipSelect pin
 
-    //* Analog Sensors
-    constexpr int ANALOG_SENSOR_0_PIN = 36;
-    constexpr int ANALOG_SENSOR_1_PIN = 39;
-
+    //! Analog Setup
+    /**
+     * @brief Enumeration for analog sensor types.
+     * This enum defines the types of analog sensors that can be connected to the system.
+     */
     enum class AnalogSensorType
     {
         None,
@@ -83,12 +39,37 @@ namespace Config
         Generic
     };
 
-    constexpr AnalogSensorType ANALOG_SENSOR_0_TYPE = AnalogSensorType::CapacitiveMoisture;
-    constexpr AnalogSensorType ANALOG_SENSOR_1_TYPE = AnalogSensorType::MAX4466;
-    constexpr AnalogSensorType ANALOG_SENSOR_2_TYPE = AnalogSensorType::None;
-    constexpr AnalogSensorType ANALOG_SENSOR_3_TYPE = AnalogSensorType::None;
-    constexpr AnalogSensorType ANALOG_SENSOR_4_TYPE = AnalogSensorType::None;
+    /**
+     * @brief  Structure representing an analog sensor.
+     * This structure contains the pin number and type of the analog sensor.
+     */
+    Struct AnalogSensor[]
+    {
+        uint8_t pin;
+        AnalogSensorType type;
+    };
 
+    /**
+     * @brief  Array of analog sensors.
+     * This array contains the pin numbers and types of the analog sensors that can be connected
+     * to the system. Each sensor is represented by an instance of the AnalogSensor structure.
+     */
+    constexpr AnalogSensor AnalogSensorTable[] = {
+        {36, AnalogSensorType::CapacitiveMoisture},
+        {39, AnalogSensorType::MAX4466},
+        {-1, AnalogSensorType::None},
+        {-1, AnalogSensorType::None},
+        {-1, AnalogSensorType::None}};
+
+    //! I2C Setup
+    //* I2C Wire pins
+    constexpr int I2C_SDA_PIN = 21;
+    constexpr int I2C_SCL_PIN = 22;
+
+    /**
+     * @brief Enumeration for I2C sensor types.
+     * This enum defines the types of sensors that can be connected via I2C.
+     */
     enum class I2CSensorType
     {
         None,
@@ -98,24 +79,28 @@ namespace Config
         BH1750
     };
 
-    constexpr I2CSensorType I2C_SENSOR_0_TYPE = I2CSensorType::BMP280;
-    constexpr uint8_t I2C_SENSOR_0_ADDRESS = 0x76;
+    /**
+     * @brief Structure representing an I2C sensor.
+     * This structure contains the I2C address and type of the sensor.
+     */
+    Struct I2CSensor
+    {
+        uint8_t address;
+        I2CSensorType type;
+    };
 
-    constexpr I2CSensorType I2C_SENSOR_1_TYPE = I2CSensorType::AHT20;
-    constexpr uint8_t I2C_SENSOR_1_ADDRESS = 0x38;
+    /**
+     * @brief Array of I2C sensors.
+     * This array contains the I2C addresses and types of the sensors that can be connected
+     * to the system. Each sensor is represented by an instance of the I2CSensor structure.
+     */
+    constexpr I2CSensor I2CSensorTable[] = {
+        {0x76, I2CSensorType::BMP280},
+        {0x38, I2CSensorType::AHT20},
+        {0x68, I2CSensorType::MPU6050},
+        {0x23, I2CSensorType::BH1750}};
 
-    constexpr I2CSensorType I2C_SENSOR_2_TYPE = I2CSensorType::MPU6050;
-    constexpr uint8_t I2C_SENSOR_2_ADDRESS = 0x68;
-
-    constexpr I2CSensorType I2C_SENSOR_0_TYPE = I2CSensorType::None;
-    constexpr uint8_t I2C_SENSOR_0_ADDRESS = 0x00;
-
-    constexpr I2CSensorType I2C_SENSOR_1_TYPE = I2CSensorType::None;
-    constexpr uint8_t I2C_SENSOR_1_ADDRESS = 0x00;
-
-    constexpr I2CSensorType I2C_SENSOR_2_TYPE = I2CSensorType::None;
-    constexpr uint8_t I2C_SENSOR_2_ADDRESS = 0x00;
-
+    // Sealevel pressure in hPa
     constexpr float SEA_LEVEL_PRESSURE_HPA = 1013.25f;
 
 } // namespace Config
