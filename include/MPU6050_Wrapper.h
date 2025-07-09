f13a01af0c71eb90c754b4d3ca2f8a32d2a2dabc
Pot4too <alexbaris167@gmail.com>
2025-07-09 21:24:18 +0200

    Remade MPU6050_Wrapper.h

--- include/MPU6050_Wrapper.h
+++ include/MPU6050_Wrapper.h
@@ -1,26 +1,58 @@
 #pragma once
+
+#define LOCAL_DEBUG
+
 #include <config.h>
+#include <GenericSensorBase.h>
 #include <Adafruit_MPU6050.h>
 
-struct SensorData_MPU6050
-{
-    float ax, ay, az; // Acceleration in m/s^2
-    float gx, gy, gz; // Angular velocity in rad/s
-    float temp;       // Temperature in °C
-    bool valid;
+#if defined(LOCAL_DEBUG) || defined(GLOBAL_DEBUG_OVERRIDE)
+#define LOCAL_DEBUG_PRINT DEBUG_PRINT
+#define LOCAL_DEBUG_PRINTLN DEBUG_PRINTLN
+#else
+#define LOCAL_DEBUG_PRINT(x)
+#define LOCAL_DEBUG_PRINTLN(x)
+#endif // DEBUG
 
-    // TODO: Make class prototype align with Generic class
-
-    SensorData_MPU6050() : ax(NAN), ay(NAN), az(NAN), gx(NAN), gy(NAN), gz(NAN), temp(NAN), valid(false) {}
-};
-
-class MPU6050_Wrapper
+class MPU6050_Wrapper : public GenericI2CSensorBase
 {
 public:
-    bool begin(uint8_t address, TwoWire &wire, int32_t sensor_id);
-    SensorData_MPU6050 read();
+    struct Data
+    {
+        float ax, ay, az; // Acceleration in m/s^2
+        float gx, gy, gz; // Angular velocity in rad/s
+        float temp;       // Temperature in °C
+        SensorData_MPU6050() : ax(NAN), ay(NAN), az(NAN), gx(NAN), gy(NAN), gz(NAN), temp(NAN) {}
+    };
+
+    MPU6050_Wrapper(uint8_t _address, TwoWire &_wire);
+
+    bool begin() ovverride;
+    bool update() ovverride;
+
+    const char *getSensorName() const ovverride { return "MPU6050"; }
+    bool isValid() const { return isValid_Bool; }
+    const void *getData() const ovverride { return &data; }
+    Config::I2CSensorType getSensorType() const ovverride { return Config::I2CSensorType::MPU6050; }
+
+    const void debugPrintData() const ovverride;
+
+    bool logDataToSd(File *dataFile) ovverride;
+    void createNameHeader(File *dataFile) ovverride;
+    void createDataTypesHeader(File *dataFile) ovverride;
 
 private:
+    Adafruit_MPU6050 sensor;
+    Data data;
+
+    uint8_t i2cAddress;
+    TwoWire *wire;
+    bool isValid_Bool = false;
+    int32_t sensorID = -1;
+
     void setSenosorConfiguration();
-    Adafruit_MPU6050 mpu;
+
+    static constexpr const char *FIELD_NAMES[] = {"aX", "aY", "aZ", "gX", "gY", "gZ", "Temperature"};
+    const uint8_t numberOfUniqueData() const ovverride { return sizeof(FIELD_NAMES) / sizeof(FIELD_NAMES[0]); }
+    const char *const *namesOfData() const ovverride { return FIELD_NAMES; }
 };
